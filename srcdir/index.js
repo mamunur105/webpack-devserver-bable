ce008e08b4620bf1d64ca26f1d390c7bda78066f
mamunur105 <rmamunur105@gmail.com>
2021-01-02 00:12:39 +0600

    bable

---
+++ srcdir/index.js
@@ -0,0 +1,10 @@
+// import "./style.css";
+import "./style.scss";
+// console.log('hello ES6 by ')
+
+const fancyFunc = () => {
+  return [1, 2];
+};
+
+const [a, b] = fancyFunc();
+// console.log(a)
